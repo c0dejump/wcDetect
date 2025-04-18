77991903f1f70df7c4713378189097dd4ac6a4c9
c0dejump <codejumpgame@gmail.com>
2025-04-18 09:41:30 +0200

    Update wcdetect.py

--- wcdetect.py
+++ wcdetect.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import sys
+import sys, re
 import random, string
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
@@ -148,14 +148,50 @@ def check_cache_presence(req_ext):
     return hit_tag
 
 
+def compare_words(set1, set2):
+    """Compare deux ensembles de mots et retourne un pourcentage de similarité"""
+    if not set1 or not set2:
+        return 0.0
+
+    common = set1 & set2
+    total = set1 | set2
+
+    similarity = (len(common) / len(total)) * 100
+    return similarity
+
+
+def get_visible_text(req):
+    soup = BeautifulSoup(req.text, 'html.parser')
+    text = soup.get_text(separator=' ')
+    words = re.findall(r'\b\w+\b', text.lower())
+    return set(words)
+
+
+def compare_words(set1, set2):
+    if not set1 or not set2:
+        return 0.0
+
+    common = set1 & set2
+    total = set1 | set2
+
+    similarity = (len(common) / len(total)) * 100
+    return similarity
+
+
 def wcd_check(upe, req_path, req_base):
     for _ in range(3):
         req_ext = s.get(upe, verify=False, allow_redirects=False, timeout=10)
     cache_status = check_cache_presence(req_ext)
     if custom_headers:
         req_verify = requests.get(upe, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0"}, verify=False, allow_redirects=False, timeout=10)
-        if len(req_verify.content) == len(req_ext.content) and req_verify.status_code == req_ext.status_code and len(req_verify.content) != len(req_base.content):
-            print(f"\033[31m └── [VULNERABILITY CONFIRMED]\033[0m | Cache Deception | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
+        if req_verify.status_code == req_ext.status_code and len(req_verify.content) != len(req_base.content):
+            words1 = get_visible_text(req_verify)
+            words2 = get_visible_text(req_path)
+            similarity = compare_words(words1, words2)
+            if similarity > 15:
+                print(f"\033[31m └── [VULNERABILITY CONFIRMED]\033[0m | Cache Deception | CACHETAG: {cache_status} | {similarity:.2f}% | \033[34m{upe}\033[0m")
+            else:
+                print(f"\033[33m └── [INTERESTING BEHAVIOR]\033[0m | Cache Deception | [{req_path.status_code}] > [{req_ext.status_code}] | CACHETAG: {cache_status} | {similarity:.2f}% | \033[34m{upe}\033[0m")
     else:
         if req_ext.status_code != req_path.status_code and req_ext.status_code not in [410, 404, 403, 308]:
             print(f"\033[33m └── [INTERESTING BEHAVIOR]\033[0m | Cache Deception | HL: {len(req_path.headers)}b > {len(req_ext.headers)}b | [{req_path.status_code}] > [{req_ext.status_code}] | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
@@ -167,6 +203,7 @@ def wcd_check(upe, req_path, req_base):
 
 def wcd_making(s, url_p, req_base):
     req_path = s.get(url_p, verify=False, allow_redirects=False, timeout=10)
+    print(req_path)
     if req_path.status_code not in [410, 404, 308]:
         if req_path.status_code == 403 and req_base.status_code == 403:
             pass
@@ -182,14 +219,13 @@ def wcd_making(s, url_p, req_base):
                 f"{url_p}?format={e}", #Ex: toto.com/profile?format=pdf
                 ]
                 for upe in url_p_e:
-                    print(upe)
+                    #print(upe)
                     wcd_check(upe, req_path, req_base)
 
 def wcd_base(url, s):
     req_base = s.get(url, verify=False, allow_redirects=False, timeout=10)
     if KNOWN_PATHS:
         for np in KNOWN_PATHS:
-            print(np)
             np = np if np[0] != "/" else np[1:]
             url_p = f"{url}{np}"
             wcd_making(s, url_p, req_base)
