b76d4ad82a17892c9528c94287b155038bb53740
Nathan <nathan@debian-nathan>
2025-08-18 15:27:54 +0200

    fix

--- modules/wcd.py
+++ modules/wcd.py
@@ -5,7 +5,21 @@ from modules.utils import requests, BeautifulSoup, random, string
 from urllib.parse import urljoin, urlparse
 from modules.payloads import DEFAULT_PATHS, KNOWN_PATHS, extensions, delimiters
 from modules.compare import get_visible_text, compare_words
+from modules.utils import Identify
 import traceback
+
+block_count = 0
+
+def waf_verify(req_verify):
+    if req_verify.status_code == 429:
+        print("[I] 429 You appear to have been blocked by a WAF.")
+        sys.exit()
+    if req_verify.status_code == 403:
+        if block_count > 3:
+            print("[I] 403 You appear to have been blocked by a WAF.")
+            sys.exit()
+        else:
+            block_count += 1
 
 def check_cache_presence(req_ext):
     #print(req_ext.headers)
@@ -20,33 +34,34 @@ def check_cache_presence(req_ext):
 def wcd_check(s, upe, req_path, req_base, custom_headers, keyword):
     for _ in range(3):
         req_ext = s.get(upe, verify=False, allow_redirects=False, timeout=10)
+    waf_verify(req_ext)
     cache_status = check_cache_presence(req_ext)
     try:
         if custom_headers:
             req_verify = requests.get(upe, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0"}, verify=False, allow_redirects=False, timeout=10)
-            #if req_verify.status_code == 429: 
+            waf_verify(req_verify)
             if req_verify.status_code == req_ext.status_code and len(req_verify.content) != len(req_base.content):
                 words1 = get_visible_text(req_verify)
                 words2 = get_visible_text(req_path)
                 similarity = compare_words(words1, words2)
 
                 if similarity > 50 and not keyword:
-                    print(f"\033[31m └── [VULNERABILITY CONFIRMED]\033[0m | Cache Deception | CACHETAG: {cache_status} | [{req_ext.status_code}] | {similarity:.2f}% | \033[34m{upe}\033[0m")
+                    print(f"{Identify.confirmed} | Cache Deception | CACHETAG: {cache_status} | [{req_ext.status_code}] | {similarity:.2f}% | \033[34m{upe}\033[0m")
                 elif keyword:
                     if keyword in req_verify.text:
-                        print(f"\033[31m └── [VULNERABILITY CONFIRMED]\033[0m | Cache Deception | CACHETAG: {cache_status} | Keyword [{keyword}] present | \033[34m{upe}\033[0m")
+                        print(f"{Identify.confirmed} | Cache Deception | CACHETAG: {cache_status} | Keyword [{keyword}] present | \033[34m{upe}\033[0m")
                 elif 20 <= similarity <= 50 :
-                    print(f"\033[33m └── [INTERESTING BEHAVIOR]\033[0m | Cache Deception | [{req_path.status_code}] > [{req_ext.status_code}] | {similarity:.2f}% | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
+                    print(f"{Identify.behavior} | Cache Deception | [{req_path.status_code}] > [{req_ext.status_code}] | {similarity:.2f}% | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
         else:
             if req_ext.status_code != req_path.status_code and req_ext.status_code not in [410, 404, 403, 308]:
-                print(f"\033[33m └── [INTERESTING BEHAVIOR]\033[0m | Cache Deception | HL: {len(req_path.headers)}b > {len(req_ext.headers)}b | [{req_path.status_code}] > [{req_ext.status_code}] | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
+                print(f"{Identify.behavior} | Cache Deception | HL: {len(req_path.headers)}b > {len(req_ext.headers)}b | [{req_path.status_code}] > [{req_ext.status_code}] | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
         """
         elif len(req_ext.headers) != len(req_path.headers) and req_ext.status_code not in [410, 404, 403, 308]:
             print(f"\033[33m └── [INTERESTING BEHAVIOR]\033[0m | Cache Deception | HL: {len(req_path.headers)}b > {len(req_ext.headers)}b | CACHETAG: {cache_status} | \033[34m{upe}\033[0m")
         """
     except:
         pass
-        #traceback.print_exc()
+        traceback.print_exc()
 
 
 def path_traversal_confusion(s, url, kp, req_base, req_path, custom_headers, keyword):
